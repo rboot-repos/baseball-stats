2a7e29b3a8ff9346a11fcad61b019082bb009a4e
Ryan <131836447+rboot-repos@users.noreply.github.com>
2023-10-25 16:53:58 -0400

    Update baseballstats_analysis.py

--- baseballstats_analysis.py
+++ baseballstats_analysis.py
@@ -1,14 +1,12 @@
-# Load the data from the CSV file
 df = pd.read_csv('mlb_batting_stats_2022.csv')
 
-# Calculate descriptive statistics
 summary_stats = df.describe()
 
-# Calculate the average batting average (BA)
+# (BA)
 average_ba = df['BA'].mean()
 
-# Filter the data to find the players with the highest home run (HR) count
+# (HR) count
 top_homerun_hitters = df.nlargest(10, 'HR')
 
-# Calculate the correlation between home runs and RBIs
+# home runs and RBIs
 correlation = df['HR'].corr(df['RBI'])
